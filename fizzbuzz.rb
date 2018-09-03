8c1faf30bce192af1de44bad787aa2261d826b73
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-03 17:34:48 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(n)
+  return "FizzBuzz" if n%15==0
+  return "Fizz" if n%3==0
+  return "Buzz" if n%5==0
+  nil
+end
+  
